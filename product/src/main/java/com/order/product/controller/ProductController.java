5a55b4d1902237fb8a84ff73b63f0f067b353129
jelenarep <jelenarep@telekom.rs>
2024-11-09 17:09:37 +0100

    Added docker-compose.yaml and second version of product app

--- product/src/main/java/com/order/product/controller/ProductController.java
+++ product/src/main/java/com/order/product/controller/ProductController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
-@CrossOrigin(origins = "http://localhost:8000")
 public class ProductController {
 
     private final  ProductServiceImpl productService;
